c298f62ddc56172eb30711df85f844234f35293b
baejunil <bjo6300@naver.com>
2023-10-15 22:48:23 +0900

    add count query example (optimazation)

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -3,10 +3,12 @@ package study.querydsl.repository;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -14,6 +16,7 @@ import study.querydsl.dto.QMemberTeamDto;
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
+import study.querydsl.entity.Member;
 
 import static org.springframework.util.StringUtils.hasText;
 import static study.querydsl.entity.QMember.member;
@@ -66,9 +69,9 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetchResults();
+                .fetchResults(); // querydsl이 알아서 total count query 날려준다.
         List<MemberTeamDto> content = results.getResults();
-        long total = results.getTotal();
+        long total = results.getTotal(); // count query
 
         return new PageImpl<>(content,pageable,total);
     }
@@ -92,20 +95,37 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
-                .fetch();
-        long total = queryFactory
-                .select(member)
-                .from(member)
-                .leftJoin(member.team, team)
-                .where(
-                        usernameEq(condition.getUsername()),
-                        teamNameEq(condition.getTeamName()),
-                        ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe())
-                )
-                .fetchCount();
+                .fetch(); // fetch는 직접 total count query를 날려야한다.
 
-        return new PageImpl<>(content,pageable,total);
+//        long total = queryFactory // 1번
+//            .select(member)
+//            .from(member)
+//            .leftJoin(member.team, team) // content는 join이 필요한데, count 쿼리는 join이 필요없는 경우에 사용
+//            .where(
+//                usernameEq(condition.getUsername()),
+//                teamNameEq(condition.getTeamName()),
+//                ageGoe(condition.getAgeGoe()),
+//                ageLoe(condition.getAgeLoe())
+//            )
+//            .fetchCount();
+
+        JPAQuery<Member> countQuery = queryFactory // 2번 - 최적화
+            .select(member)
+            .from(member)
+            .leftJoin(member.team, team) // content는 join이 필요한데, count 쿼리는 join이 필요없는 경우에 사용
+            .where(
+                usernameEq(condition.getUsername()),
+                teamNameEq(condition.getTeamName()),
+                ageGoe(condition.getAgeGoe()),
+                ageLoe(condition.getAgeLoe())
+            );
+
+//        return new PageImpl<>(content,pageable,total); // 1번
+
+        // count query 최적화 (2번)
+        // 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+        // 마지막 페이지일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈를 구한다.)
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
     }
 
     private Predicate usernameEq(String username) {
